afe2a0f17eb9b482788791940e236c0e6b0734ae
EC2 Default User <ec2-user@ip-172-31-24-112.ec2.internal>
2020-02-08 01:45:56 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,6 @@ class MoviesController < ApplicationController
   end
 
   def index
-  
     @all_ratings = Movie.all_ratings
     @selectRatings = @all_ratings
     @movies = Movie.where(rating: @selectRatings)
